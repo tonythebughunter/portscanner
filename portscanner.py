27bd4957a08c2c0849becf5f851cb9378a69b032
tonythebughunter <144837471+tonythebughunter@users.noreply.github.com>
2024-05-02 11:56:10 +0300

    Add files via upload

---
+++ portscanner.py
@@ -0,0 +1,31 @@
+import socket
+import sys
+
+def portscan(ipaddress, port):
+    try:
+        sock = socket.socket()
+        sock.settimeout(0.05)
+        sock.connect((ipaddress, port))
+        print("Port " + str(port) + " is open")
+    except socket.timeout:
+        pass
+    except KeyboardInterrupt:
+        print("\nScan interrupted by user.")
+        sys.exit()  # Exit the script
+    finally:
+        sock.close()
+
+try:
+    ipaddress = input("[+] Enter the target IP address or URL: ")
+    a = int(input("[+] Enter the first port to scan: "))
+    b = int(input("[+] Enter the final port to scan: "))
+    print("Scanning " + str(ipaddress))
+
+    for port in range(a, b):
+        portscan(ipaddress, port)
+
+except KeyboardInterrupt:
+    print("\nScan interrupted by user.")
+    sys.exit()  # Exit the script
+
+     
